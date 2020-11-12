ceb75a1af2f7918d94fe0491d96a8d1192ffc067
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-12 20:22:56 +0100

    Added ability to charsheet template

--- src/app/charsheet/abilities.component.ts
+++ src/app/charsheet/abilities.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-abilities',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./abilities.component.css']
 })
 export class AbilitiesComponent implements OnInit {
+
+  @Input() ablist: any[] ;
 
   constructor() { }
 
