61b3db12ffc6fb4ca26f34a651be07d93f756c43
Hans Georg Schaathun <georg@schaathun.net>
2020-11-13 18:55:54 +0000

    Adapted charsheet and characteristic to use the new CharacteristicList type.

--- src/app/charsheet/characteristics.component.ts
+++ src/app/charsheet/characteristics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CharacteristicList } from '../characteristic';
 
 @Component({
   selector: 'app-characteristics',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CharacteristicsComponent implements OnInit {
 
-  @Input() charlist: any[] ;
+  @Input() charlist: CharacteristicList ;
 
   constructor() { }
 
