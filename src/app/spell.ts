d8a452d044c5a4ad8b06f14844873b52a53144d4
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-15 11:04:37 +0100

    Added range/duration/target

--- src/app/spell.ts
+++ src/app/spell.ts
@@ -4,10 +4,16 @@ export class Spell {
 
     spellName: string ;
     raw: any ;
+    range: string ;
+    duration: string ;
+    target: string ;
     constructor(ob) {
       console.log( ob ) ;
       this.spellName = ob["rdfs:label"] ;
       this.raw = ob ;
+      this.range = ob["arm:hasRangeString"] || "";
+      this.duration = ob["arm:hasDurationString"] || "";
+      this.target = ob["arm:hasTargetString"] || "";
     }
     getSignature() : string {
       return "" ;
