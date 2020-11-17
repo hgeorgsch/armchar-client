5536c48247984b4fcccf8aff72367e5432d8180a
Hans Georg Schaathun @kerckhoffs <georg@schaathun.net>
2020-11-17 09:48:26 +0100

    Another component stub.

--- src/app/advancement/advancement.module.ts
+++ src/app/advancement/advancement.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdvancementComponent } from './advancement.component';
+import { CharacterAdvancementComponent } from './character-advancement.component';
 
 
 
 @NgModule({
-  declarations: [AdvancementComponent],
+  declarations: [AdvancementComponent, CharacterAdvancementComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/advancement/character-advancement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-advancement',
+  templateUrl: './character-advancement.component.html',
+  styleUrls: ['./character-advancement.component.css']
+})
+export class CharacterAdvancementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
