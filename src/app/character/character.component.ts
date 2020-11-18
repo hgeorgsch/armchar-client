1ec44c85fe51718690e2e4d50d63f09a384860b7
Hans Georg Schaathun <georg@schaathun.net>
2020-11-18 15:50:04 +0000

    New character view compiles.  Does not display.

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ArmcharService } from '../armchar.service';
-import { Character, characterParse } from '../charsheet';
+import { Character, characterParse, Charsheet, charsheetParse } from '../charsheet';
 import { switchMap } from 'rxjs/operators';
 
 @Component({
@@ -14,7 +14,8 @@ export class CharacterComponent implements OnInit {
   char : string = "cieran" ;
   time : string ;
   character: Character | undefined ;
-  subs: any ;
+  subs1: any ;
+  subs2: any ;
 
   constructor( 
        private armcharService: ArmcharService,
@@ -22,13 +23,18 @@ export class CharacterComponent implements OnInit {
        ) { }
 
   ngOnInit(): void {
-     this.subs = this.route.queryParams.pipe(
-	 switchMap( params => { this.char = params['char'] ; 
-	   this.time = params['time'] || undefined ; 
-           return this.armcharService.getCharacter( this.char ) 
-    }) )
+     var pipe =
+       this.route.queryParams.pipe(
+	 switchMap( params => { this.char = params['char'] ;
+	   this.time = params['time'] || undefined ;  } ) ) ;
+     this.subs1 =  pipe.pipe( () => { 
+           return this.armcharService.getCharacter( this.char ) }) 
 	 .subscribe( cs => { console.log(cs) ;
-	    return this.character = characterParse( cs ) } ) 
+	    return this.character = characterParse( cs ) } ) ;
+     this.subs2 =  pipe.pipe( () => { 
+           return this.armcharService.getCharsheet( this.char, this.time ) }) 
+	 .subscribe( cs => { console.log(cs) ;
+	    return this.charsheet = charsheetParse( cs ) } ) ;
 
   }
 
