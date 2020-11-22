af8a818159d0f766735aaa92755e7425317ff342
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-22 12:32:13 +0100

    bugfixes

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ArmcharService } from '../armchar.service';
 import { Character, characterParse, Charsheet, charsheetParse } from '../charsheet';
 import { switchMap } from 'rxjs/operators';
-import {Observable,of, from } from 'rxjs';
+import {Observable, EMPTY,of, from } from 'rxjs';
 
 interface Params { char: string, time: string,  year: number, season: string }
 
@@ -26,10 +26,15 @@ export class CharacterComponent implements OnInit {
   params$: Observable<Params> ;
 
   storeParams( params: any ) : Params {
-     this.char = params['char'] ;
-     this.time = params['time']  ;
-     this.year = params['year']  ;
-     this.season = params['season']  ;
+     console.log( "storeParams:", params ) ;
+     if ( typeof(params['char']) !== "undefined" )
+        this.char = params['char'] ;
+     if ( typeof(params['time']) !== "undefined" )
+        this.time = params['time']  ;
+     if ( typeof(params['year']) !== "undefined" )
+        this.year = params['year']  ;
+     if ( typeof(params['season']) !== "undefined" )
+        this.season = params['season']  ;
      console.log( "character:", this.char, this.time, this.year, this.season ) ;
      return {
         "char" : this.char,
@@ -47,12 +52,16 @@ export class CharacterComponent implements OnInit {
   ngOnInit(): void {
      console.log( "character starting:", this.char, this.time ) ;
      this.params$ = this.route.queryParams.pipe( 
-               params => { return of(this.storeParams(params) ) } ) ;
+            switchMap( params => { return of(this.storeParams(params) ) } ) ) ;
      this.subs1 =  this.params$.pipe( switchMap( (x) => {
            return this.armcharService.getCharacter( this.char ) }))
 	 .subscribe( cs => { console.log("character", cs) ;
 	    this.character = characterParse( cs ) } ) ;
      this.subs2 =  this.params$.pipe( switchMap( (x) => { 
+         if ( !this.char ) return EMPTY ;
+         if ( (!this.year) || (!this.season) ) return EMPTY ;
+         if ( typeof(this.year) === "undefined" ) return EMPTY ;
+         if ( typeof(!this.season) === "undefined" ) return EMPTY ;
          return this.armcharService.getCharsheet( this.char,
                           this.year, this.season ) }) )
 	 .subscribe( cs => { console.log("charsheet", cs) ;
